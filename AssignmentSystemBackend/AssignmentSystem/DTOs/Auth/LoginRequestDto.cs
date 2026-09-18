549d56970bd861f97362c50cfe5dee68893927dc
Aznan Seyam <106177845+AznanSeyam@users.noreply.github.com>
2026-09-18 10:07:48 +0600

    Update LoginRequestDto.cs

--- AssignmentSystemBackend/AssignmentSystem/DTOs/Auth/LoginRequestDto.cs
+++ AssignmentSystemBackend/AssignmentSystem/DTOs/Auth/LoginRequestDto.cs
@@ -4,6 +4,7 @@ namespace AssignmentSystemBackend.DTOs.Auth
     public class LoginRequestDto
     {
         [Required, EmailAddress]
+        
         public string Email { get; set; } = string.Empty;
         [Required]
         public string Password { get; set; } = string.Empty;
